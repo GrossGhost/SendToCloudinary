0bfab25a2ff8b4730aef8a2295e3af420b9cf9c5
GrossGhost <grossghost13@gmail.com>
2017-06-09 12:48:47 +0300

    login With FaceBook

--- app/src/main/java/com/example/gross/sendtocloudinary/MainActivity.java
+++ app/src/main/java/com/example/gross/sendtocloudinary/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.gross.sendtocloudinary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private TextView txtLogin;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        loginButton = (LoginButton) findViewById(R.id.btn_login);
+        txtLogin = (TextView) findViewById(R.id.txtLogin);
+        callbackManager = CallbackManager.Factory.create();
+
+        loginWithFC();
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    private  void loginWithFC(){
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                txtLogin.setText("Login Success \n" +
+                        loginResult.getAccessToken().getUserId() +
+                        "\n" + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                txtLogin.setText("Login Canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                txtLogin.setText("Login Error \n" + error);
+            }
+        });
+    }
+
 }
